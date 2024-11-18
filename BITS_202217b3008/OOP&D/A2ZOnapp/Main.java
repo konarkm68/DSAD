3ce4a447462be3983765c027da7bfa3892e593e0
Konark Mittal <49273687+konarkm68@users.noreply.github.com>
2024-11-18 16:14:10 +0530

    upd changes

--- BITS_202217b3008/OOP&D/A2ZOnapp/Main.java
+++ BITS_202217b3008/OOP&D/A2ZOnapp/Main.java
@@ -3,11 +3,15 @@ package A2ZOnapp;
 public class Main
 {
 	public static void main(String[] args) {
+		// New user
 		regd_user user = new regd_user();
+		// Get user details, validate them and register user
 		user.get_user_details();
-
+		
+		// Browse Shop Products
         product_info.display_product_details();
-
+        
+        // Checkout
         cust_billing.checkout();
 	}
 }
